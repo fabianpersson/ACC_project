eef277e8dae17dfb015162abefc416091f206047
Your Name <you@example.com>
2018-10-29 12:31:10 +0000

    added configurations for cluster deployment

--- container/shared/proj/celeryconfig.py
+++ container/shared/proj/celeryconfig.py
@@ -1,4 +1,4 @@
-broker_url = 'amqp://guest:guest@rabbit:5672/'
+broker_url = 'amqp://fabian:fabian@rabbit:5672/'
 broker_pool_limit = 1 # Will decrease connection usage
 broker_heartbeat = None # We're using TCP keep-alive instead
 broker_connection_timeout = 30 # May require a long timeout due to Linux DNS timeouts etc
